300d0507773873f52a4acda0eb2754049c827458
unknown <laura.caicedoprieto@gmail.com>
2021-10-13 11:41:31 -0500

    Inicio despliegue

---
+++ neverappApp/serializers/userSerializer.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from neverappApp.models.user import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'name', 'email']
+
+       
+
+        
