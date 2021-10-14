93ead33ef2e2fc219147b46d27da9dd20cac363a
unknown <laura.caicedoprieto@gmail.com>
2021-10-13 21:31:30 -0500

    Tercer despliegue

--- neverappApp/serializers/userSerializer.py
+++ neverappApp/serializers/userSerializer.py
@@ -8,6 +8,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'password', 'name', 'email']
 
-       
 
-        
+    def create(self, validated_data):
+        userInstance = User.objects.create(**validated_data)
+        return userInstance       
+
+    def to_representation(self, obj):
+        user = User.objects.get(id=obj.id)
+            
+        return {
+                    'id': user.id, 
+                    'username': user.username,
+                    'name': user.name,
+                    'email': user.email,
+                }       
