bc4092ebe1cdec5d6afe56bc084be52cf9af10db
calzateu <calzateu@eafit.edu.co>
2021-10-20 15:36:48 -0500

    Invertí el orden de los related_name en las tablas, ya que vi más detenidamente un video y lo consideré correcto

--- neverapp_be/neverappApp/models/UsuarioHasMenu.py
+++ neverapp_be/neverappApp/models/UsuarioHasMenu.py
@@ -3,5 +3,5 @@ from .user import User
 from .menu import Menu
 
 class UsuarioHasMenu(models.Model):
-    user = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE)
-    menu = models.ForeignKey(Menu, related_name='menus', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='menus', on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, related_name='users', on_delete=models.CASCADE)
--- neverapp_be/neverappApp/models/menuHasIngredients.py
+++ neverapp_be/neverappApp/models/menuHasIngredients.py
@@ -2,6 +2,6 @@ from django.db import models
 from .ingredients import Ingredient
 from .menu import Menu
 
-class MenuMenu(models.Model):
-    menu = models.ForeignKey(Menu, related_name='menus', on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, related_name='ingredients', on_delete=models.CASCADE)
+class menuHasIngredient(models.Model):
+    menu = models.ForeignKey(Menu, related_name='ingredients', on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, related_name='menus', on_delete=models.CASCADE)
