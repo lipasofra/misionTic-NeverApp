51683467e748a4afae84f8c44de2b703cafb4654
calzateu <calzateu@eafit.edu.co>
2021-10-21 13:19:37 -0500

    Ensayo

--- neverapp_be/neverappProject/urls.py
+++ neverapp_be/neverappProject/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view()),
     path('user/', views.UserCreateView.as_view()),
     path('user/<int:pk>/', views.UserDetailView.as_view()),
-    path('createIngredient/', views.IngredientCreateView),
+    path('createIngredient/', views.IngredientCreateView.as_view()),
     path('createMenu/', views.MenuCreateView.as_view()),
     path('createMenuHasIngredient/', views.MenuHasIngredientsCreateView.as_view()),
     path('createUserHasMenu/', views.UserHasMenuCreateView.as_view()),
