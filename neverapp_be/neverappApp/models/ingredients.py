5b9786c3ebcd9d1d86564266e328d6aaeffc2f90
lipasofra <lipasofra.ph@gmail.com>
2021-10-20 21:19:19 -0500

    q raro no hice cambios

--- neverapp_be/neverappApp/models/ingredients.py
+++ neverapp_be/neverappApp/models/ingredients.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Ingredient(models.Model):
     id = models.AutoField(primary_key=True)
-    nameMenu = models.CharField('Name Ingredient', max_length=100)
+    nameIngrediente = models.CharField('Name Ingredient', max_length=100)
--- neverapp_be/neverappApp/models/menu.py
+++ neverapp_be/neverappApp/models/menu.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Menu(models.Model):
     id = models.AutoField(primary_key=True)
-    nameMenu = models.CharField('Name Menu', max_length=100)
+    nameMenu = models.CharField('Name Menu', max_length=100, null=False, blank=False)
     timeMenu = models.IntegerField(default=0)
     levelMenu = models.CharField('Level', max_length=10)
