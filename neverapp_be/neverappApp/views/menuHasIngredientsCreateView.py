5adaaf15ee49b76cc65f8b153d193424b14a7b63
calzateu <calzateu@eafit.edu.co>
2021-10-21 10:42:05 -0500

    Guardando de nuevo. Algunos cambios no se habían guardado bien

--- neverapp_be/neverappApp/views/menuHasIngredientsCreateView.py
+++ neverapp_be/neverappApp/views/menuHasIngredientsCreateView.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from neverappApp.serializers.menuHasIngredientsSerializer import MenuHasIngredientSerializer
 
 
-class MenuCreateView(views.APIView):
+class MenuHasIngredientsCreateView(views.APIView):
     def post(self, request, *args, **kwargs):
         serializer = MenuHasIngredientSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
