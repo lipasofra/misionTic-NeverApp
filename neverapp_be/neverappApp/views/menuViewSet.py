ec39054fae74c4d9031c9301f8a8f8aae0c4b08a
calzateu <calzateu@eafit.edu.co>
2021-10-27 22:19:33 -0500

    Filtro de ingredientes para menus

--- neverapp_be/neverappApp/views/menuViewSet.py
+++ neverapp_be/neverappApp/views/menuViewSet.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from rest_framework import status, views
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -5,13 +6,16 @@ from neverappApp.models.ingredients import Ingredient
 from neverappApp.models.menu import Menu
 from neverappApp.serializers.menuSerializer import MenuSerializer
 
-
 class MenuViewSet(viewsets.ModelViewSet):
     serializer_class = MenuSerializer
 
     def get_queryset(self):
-        menus = Menu.objects.all()
-        return menus
+        querySet = Menu.objects.all()
+        ingredient = self.request.query_params.get('ingredient', None)
+        if ingredient is not None:
+            querySet = querySet.filter(ingredients=ingredient)
+
+        return querySet
 
     def create(self, request, *args, **kwargs):
         data = request.data
